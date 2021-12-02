cd390186427b56e918a5e3bd0c5c2bcfa81dd78b
dereEnes <dere.enes01@gmail.com>
2021-12-02 12:01:45 +0300

    Add project files.

---
+++ Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika.ortalamaHesaplama
+{
+    class Fibonacci
+    {
+        public static double Calculate(int number)
+        {
+            int[] arrayOfFibo = new int[number];
+            arrayOfFibo[0] = 0;
+            arrayOfFibo[1] = 1;
+            int sum = 1;
+            for (int i = 2; i< number; i++)
+            {
+                arrayOfFibo[i] = arrayOfFibo[i - 1] + arrayOfFibo[i - 2];
+                sum += arrayOfFibo[i];
+            }
+            double a =  (double)sum / (double)number;
+            return a;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace patika.ortalamaHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number => ");
+            int number = 0;
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine((Fibonacci.Calculate(number)).ToString("N2"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
